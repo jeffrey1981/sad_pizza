617ed10d7996d3c75c70a3bc0cd89f1926efac1e
Jeffrey Lewis <jeffersonslewis@gmail.com>
2015-11-02 14:03:25 -0800

    update user auth coding

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "You are now cheesy!"
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+    end
+end
